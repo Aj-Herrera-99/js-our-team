6675ab4abaa39889a6e308a2afaa364ffb4390b8
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-13 15:36:43 +0100

    loading logic with js

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,15 +6,16 @@ creare una pagina dedicata  in cui mostrare una card per ciascun componente.
 // DOM elements selection
 const cardWrapper = document.getElementById("card-wrapper");
 const memberForm = document.getElementById("member-form");
+const formContainer = document.getElementById("form-container");
 // generate all cards
-for(const member of teamMembers){
+for (const member of teamMembers) {
     cardWrapper.insertAdjacentHTML("beforeend", generateCard(member));
 }
 // event listeners
 memberForm.addEventListener("submit", handleSubmit);
 
 //event handlers
-function handleSubmit(e){
+function handleSubmit(e) {
     e.preventDefault();
     const fname = document.getElementById("fname").value;
     const lname = document.getElementById("lname").value;
@@ -22,31 +23,57 @@ function handleSubmit(e){
     const email = document.getElementById("email").value;
     const image = "img/male1.png";
     const newMember = createNewMember(fname, lname, role, email, image);
-    console.log(newMember);
+    formContainer.insertAdjacentHTML(
+        "beforeend",
+        `<p id="sending">Sending . </p>`
+    );
+    const sending = document.getElementById("sending");
+    let count = 1;
+    const bound = 3;
+    const clock = setInterval(() => {
+        count++;
+        if (count <= bound) {
+            sending.append(" .");
+        } else {
+            sending.innerHTML = "Sending .";
+            count = 1;
+        }
+    }, 350);
+
+    setTimeout(() => {
+        clearInterval(clock);
+        sending.innerHTML = `Congratulations! Your application has been accepted!<br>
+        You're parte of the team now!`
+    }, 2500);
 }
 
 /**
- * 
- * @param {teamMembers[0]} cardObj 
- * @returns 
+ *
+ * @param {teamMembers[0]} cardObj
+ * @returns
  */
-function generateCard(cardObj){
+function generateCard(cardObj) {
     return `<div class="card">
                 <div class="image">
-                    <img src="./assets/${cardObj.img}" alt="${(cardObj.img).substring(0, cardObj.img.length-4)}" />
+                    <img src="./assets/${
+                        cardObj.img
+                    }" alt="${cardObj.img.substring(
+        0,
+        cardObj.img.length - 4
+    )}" />
                 </div>
                 <span>${cardObj.name}</span>
                 <span>${cardObj.role}</span>
                 <span>${cardObj.email}</span>
             </div>
-        </div>`
+        </div>`;
 }
 
-function createNewMember(fname, lname, role, email, image){
+function createNewMember(fname, lname, role, email, image) {
     return {
         name: fname + " " + lname,
         role: role,
         email: email,
         img: image,
-      }
+    };
 }
